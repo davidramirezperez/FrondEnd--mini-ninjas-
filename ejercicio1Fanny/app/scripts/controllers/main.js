d1a3d1529ad809a7733df761a176ad247d4b3e08
Fanny Solano <fannys@thehangar.cr>
2013-10-17 15:51:13 -0600

    proyecto de Fanny

---
+++ ejercicio1Fanny/app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('miniNinjasApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
